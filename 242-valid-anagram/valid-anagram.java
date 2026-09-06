a9042693ecd8177df4673ab47451107f5b25f1f9
coderr-nishant <nishantmaurya0210@gmail.com>
2026-09-06 12:05:17 +0530

    Time: 17 ms (16.90%) | Memory: 46.6 MB (16.16%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+        }
+        for(int i=0;i<t.length();i++){
+            if(!map.containsKey(t.charAt(i))){
+                return false;
+            }else{
+                map.put(t.charAt(i),map.get(t.charAt(i))-1);
+            }
+        }
+        for(Map.Entry<Character,Integer> entry:map.entrySet()){
+            if(entry.getValue()!=0)
+            return false;
+        }
+        return true;
+    }
+}
